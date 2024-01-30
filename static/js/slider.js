931c38a08eb12b2e74c4987ac6c04e3c442078e8
Xbasan <121888389+Xbasan@users.noreply.github.com>
2024-01-30 20:25:54 +0300

    обнова

--- static/js/slider.js
+++ static/js/slider.js
@@ -20,32 +20,12 @@ function initializeSlider(selector, slides) {
 
 }
 
-const slides1 = document.querySelectorAll('.slide_img_1');
-const slides2 = document.querySelectorAll('.slide_img_2');
-const slides3 = document.querySelectorAll('.slide_img_3');
-const slides4 = document.querySelectorAll('.slide_img_4');
-const slides5 = document.querySelectorAll('.slide_img_5');
-const slides6 = document.querySelectorAll('.slide_img_6');
-const slides7 = document.querySelectorAll('.slide_img_7');
-const slides8 = document.querySelectorAll('.slide_img_8');
-const slides9 = document.querySelectorAll('.slide_img_9');
-const slides10 = document.querySelectorAll('.slide_img_10');
-const slides11 = document.querySelectorAll('.slide_img_11');
-const slides12 = document.querySelectorAll('.slide_img_12');
-const slides13 = document.querySelectorAll('.slide_img_13');
+const post = document.querySelectorAll('.post')
 
-initializeSlider('post_1', slides1);
-initializeSlider('post_2', slides2);
-initializeSlider('post_3', slides3);
-initializeSlider('post_4', slides4);
-initializeSlider('post_5', slides5);
-initializeSlider('post_6', slides6);
-initializeSlider('post_7', slides7);
-initializeSlider('post_8', slides8);
-initializeSlider('post_9', slides9);
-initializeSlider('post_10', slides10);
-initializeSlider('post_11', slides11);
-initializeSlider('post_12', slides12);
-initializeSlider('post_13', slides13);
+post.forEach((el, index)=>{
+    initializeSlider(`post_${index+1}`,
+    document.querySelectorAll(`.slide_img_${index+1}`))
+})
+
 
 
